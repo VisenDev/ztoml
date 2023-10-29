418b8a5474bf2bed8be434626171b02e7dc3bd75
Robert Burnett <dr.slate2@gmail.com>
2023-10-28 19:14:06 -0500

    refactored build.zig

--- build.zig
+++ build.zig
@@ -5,11 +5,8 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "ztoml",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+    _ = b.addModule("ztoml", .{
+        .source_file = .{ .path = "src/main.zig" },
     });
 
     // =============TOML TO JSON LINKING=================
@@ -19,17 +16,6 @@ pub fn build(b: *std.Build) void {
         "--release",
     });
     b.default_step.dependOn(&rust_compile.step);
-    var toml_to_json = b.addStaticLibrary(.{
-        .name = "toml-to-json",
-        .optimize = optimize,
-        .target = target,
-    });
-    toml_to_json.step.dependOn(&rust_compile.step);
-    toml_to_json.addObjectFile(.{ .path = "./target/release/libtoml_to_json.a" });
-    lib.linkLibrary(toml_to_json);
-    lib.addIncludePath(.{ .path = "toml-to-json" });
-
-    b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
@@ -46,3 +32,73 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
 }
+
+pub fn link(step: *std.build.CompileStep) void {
+    step.addLibraryPath(.{ .path = "target/release" });
+    step.linkSystemLibrary("toml_to_json");
+    step.addIncludePath(.{ .path = "src" });
+}
+
+//const std = @import("std");
+//
+//pub fn build(b: *std.Build) void {
+//    const target = b.standardTargetOptions(.{});
+//
+//    const optimize = b.standardOptimizeOption(.{});
+//
+//    _ = b.addModule("ztoml", .{
+//        .source_file = .{ .path = "src/main.zig" },
+//    });
+//
+//    const lib = b.addStaticLibrary(.{
+//        .name = "ztoml",
+//        .root_source_file = .{ .path = "src/main.zig" },
+//        .target = target,
+//        .optimize = optimize,
+//    });
+//
+//    // =============TOML TO JSON LINKING=================
+//    const rust_compile = b.addSystemCommand(&[_][]const u8{
+//        "cargo",
+//        "build",
+//        "--release",
+//    });
+//    b.default_step.dependOn(&rust_compile.step);
+//    var toml_to_json = b.addStaticLibrary(.{
+//        .name = "toml-to-json",
+//        .optimize = optimize,
+//        .target = target,
+//    });
+//    toml_to_json.step.dependOn(&rust_compile.step);
+//    toml_to_json.addObjectFile(.{ .path = "./target/release/libtoml_to_json.a" });
+//    lib.linkLibrary(toml_to_json);
+//    lib.addIncludePath(.{ .path = "toml-to-json" });
+//
+//    link(b, lib);
+//
+//    b.installArtifact(lib);
+//
+//    const main_tests = b.addTest(.{
+//        .root_source_file = .{ .path = "src/main.zig" },
+//        .target = target,
+//        .optimize = optimize,
+//    });
+//
+//    main_tests.addLibraryPath(.{ .path = "target/release" });
+//    main_tests.linkSystemLibrary("toml_to_json");
+//    main_tests.addIncludePath(.{ .path = "src" });
+//    main_tests.step.dependOn(b.getInstallStep());
+//    link(b, main_tests);
+//
+//    const run_main_tests = b.addRunArtifact(main_tests);
+//    const test_step = b.step("test", "Run library tests");
+//    test_step.dependOn(&run_main_tests.step);
+//}
+//
+//pub fn link(b: *std.Build, step: *std.build.CompileStep) void {
+//    const ztoml_dep = b.dependency("ztoml", .{
+//        .target = step.target,
+//        .optimize = step.optimize,
+//    });
+//    step.linkLibrary(ztoml_dep.artifact("ztoml"));
+//}
