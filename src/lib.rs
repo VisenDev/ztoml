a54d2c8eb4dd4802dcd7310a23a15543eb9c07a7
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-10-29 03:58:39 +0300

    fix return type

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::ffi::CString;
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern "C" fn tomlToJson(input: *const c_char) -> *const c_char {
+pub extern "C" fn tomlToJson(input: *const c_char) -> *mut c_char {
     use std::ffi::CStr;
     let c_str: &CStr = unsafe { CStr::from_ptr(input) };
     let str_slice: &str = c_str.to_str().unwrap();
